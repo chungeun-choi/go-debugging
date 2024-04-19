9cb629610325abd825f0352ad034f80b6f41be25
Chungeun Choi <ce.choi@okestro.com>
2024-04-19 10:24:19 +0900

    example - runtime debugging with live reloading

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/chungeun-choi/go-debugging
+
+go 1.22.0
---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+const port = "2024"
+
+func main() {
+	http.HandleFunc("/debug", debugHandler)
+	log.Fatalln(http.ListenAndServe(":"+port, nil))
+}
+
+func debugHandler(w http.ResponseWriter, r *http.Request) {
+	data, ok := r.URL.Query()["num"]
+	if !ok || len(data) < 0 {
+		log.Println("param is missing")
+		return
+	}
+
+	number, err := strconv.Atoi(data[0])
+	if err != nil {
+		log.Printf("invalid parameter: '%v'", err)
+		return
+	}
+
+	_, err = w.Write([]byte(fmt.Sprintf("input number: %v", number)))
+	if err != nil {
+		log.Printf("failed to return")
+		log.Println("추가")
+		return
+	}
+}
